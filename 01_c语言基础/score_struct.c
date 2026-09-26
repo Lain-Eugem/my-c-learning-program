a8d2df997312c868d3b6779d2039226f2390b99b
Lain-Eugem <lain_eugem@qq.com>
2026-09-26 17:08:04 +0800

    update

--- 01_c语言基础/score_struct.c
+++ 01_c语言基础/score_struct.c
@@ -1,14 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-typedef struct student {
+typedef struct Student {
     char name[20];
-    int age;
-} student;
+    int score;
+} Student;
 
 int main(){
     int N;
     printf("请输入学生人数：\n");
-    scanf("%d",N);
+    if (scanf("%d",&N) != 1 || N <= 0) {
+    printf("输入无效\n");
+    return 1;
+    }
 
-    
+    Student *student = malloc(N * sizeof(*student));
+    if (student == NULL){
+        printf("内存不足，申请失败。");
+        return 1;
+    }
+
+    for (int i = 0 ; i < N ; i++) {
+        printf("请输入第%d位学生的姓名：\n",i + 1);
+        scanf("%19s",student[i].name);
+
+        printf("请输入第%d位学生的成绩：\n",i + 1);
+        scanf("%d",&student[i].score);
+    }
+
+    for (int i = 0 ; i < N ; i++) {
+        printf("学生的姓名：%19s\n",student[i].name);
+        printf("学生的成绩：%d\n",student[i].score);
+    }
+
+    free(student);
+    student = NULL;
+
+    return 0;
 }
